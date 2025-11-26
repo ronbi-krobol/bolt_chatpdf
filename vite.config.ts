7a2e23dae0cfd749f269ef6a841f270f80ba1630
ronbi-krobol <ronbi.krobol@gmail.com>
2025-11-27 01:47:13 +0800

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/bolt_chatpdf/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
